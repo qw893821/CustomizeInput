47e4bfb86379da3cc9fbbb914a2c521ebfcf7da9
qw893821 <xiuman893821@gmail.com>
2018-03-04 20:50:30 -0500

    scene1
    basic customize move. lake of physic respond like gravity

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    private static GameManager _instance;
+    public static GameManager GM
+    {
+        get { return _instance; }
+    }
+
+    public string lKey;
+    public string rKey;
+    public string uKey;
+    public string dKey;
+	// Use this for initialization
+	void Start () {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else { Destroy(this); }
+        //set default value of key
+        lKey = "j";
+        rKey = "l";
+        uKey = "i";
+        dKey = "k";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    float speed = 5f;
+    float x;
+    float y;
+    float z;
+    // Use this for initialization
+    void Start () {
+        x = 0;
+        y = 0;
+        z = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 target;
+        target = transform.position + MoveDirection();
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        print(target);
+	}
+
+    Vector3 MoveDirection()
+    {
+        if (Input.GetKey(GameManager.GM.lKey) || Input.GetKey(GameManager.GM.rKey))
+        {
+            if (Input.GetKey(GameManager.GM.lKey))
+            {
+                x = -1f;
+            }
+            if (Input.GetKey(GameManager.GM.rKey))
+            {
+                x = 1f;
+            }
+        }
+        else { x = 0; }
+        if (Input.GetKey(GameManager.GM.uKey) || Input.GetKey(GameManager.GM.dKey))
+        {
+            if (Input.GetKey(GameManager.GM.uKey))
+            {
+                z = 1f;
+            }
+            if (Input.GetKey(GameManager.GM.dKey))
+            {
+                z = -1f;
+            }
+        }
+        else { z = 0; }
+        return new Vector3(x, y, z);
+    }
+}
